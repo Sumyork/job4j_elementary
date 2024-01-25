e1ae912327dfbb1f7e5626a7b787ba1ea84080e8
Sumyork <psychohistorian.p@gmail.com>
2024-01-25 14:17:23 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Pasha Leshkevich");
+        System.out.println("15.11.1987");
+    }
+}
