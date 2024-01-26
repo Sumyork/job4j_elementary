a7c7c165de8eb229a5e1b234b999e532c7242d43
Sumyork <psychohistorian.p@gmail.com>
2024-01-27 00:00:54 +0300

    4.6.1. Switch [#334070]

---
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,39 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+
+    public static String nameOfDay(int day) {
+        String name = null;
+
+        switch (day) {
+            case 1:
+                name = "понедельник";
+                break;
+            case 2:
+                name = "Вторник";
+                break;
+            case 3:
+                name = "Среда";
+                break;
+            case 4:
+                name = "Четверг";
+            case 5:
+                name = "Пятница";
+                break;
+            case 6:
+                name = "Суббота";
+                break;
+            case 7:
+                name = "Воскресение";
+                break;
+            default:
+                return "Ошибка!";
+        }
+        return name;
+    }
+
+    public static void main(String[] args) {
+        String s = nameOfDay(8);
+        System.out.println(s);
+    }
+}
