1f87895144fa1bf0002373b822b80b1d2020b8f7
Sumyork <psychohistorian.p@gmail.com>
2024-01-26 15:22:13 +0300

    1. Модульные тесты. [#159435]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.converter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.withPrecision;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert350RblThen5Euro() {
+        float input = 350;
+        float expected = 5;
+        float output = Converter.rubleToEuro(input);
+        float value = 0.0001f;
+        Assertions.assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert240RblThen4Dollar() {
+        float input = 240;
+        float expected = 4;
+        float output = Converter.rubleToDollar(input);
+        float value = 0.0001f;
+        Assertions.assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+}
