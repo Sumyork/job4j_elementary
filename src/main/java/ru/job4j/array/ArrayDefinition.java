79fa5df81216d17c24ded160e2a4516d173185a8
Sumyork <psychohistorian.p@gmail.com>
2024-01-27 22:59:25 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,17 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surname равен: " + surname.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        System.out.println();
+
+        String[] names = new String[4];
+        names[0] = "Patrick Mahomes";
+        names[1] = "Joe Burrow";
+        names[2] = "Aaron Rodgers";
+        names[3] = "Josh Allen";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
