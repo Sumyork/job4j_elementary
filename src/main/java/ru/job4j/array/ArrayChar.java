3050123efecedf3d355913504475d541bfcbe9ee
Sumyork <psychohistorian.p@gmail.com>
2024-01-29 08:42:31 +0300

    6.4.0. Слово начинается с ... [#41585 #477182] - Изменён

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,14 +4,10 @@ public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
-        for (int i = 0; i < word.length; i++) {
-            for (int j = 0; j < prefix.length; j++) {
-                if (i == j) {
-                    if (word[i] != prefix[j]) {
-                        result = false;
-                        break;
-                    }
-                }
+        for (int i = 0; i < prefix.length; i++) {
+            if (word[i] != prefix[i]) {
+                result = false;
+                break;
             }
         }
         return result;
