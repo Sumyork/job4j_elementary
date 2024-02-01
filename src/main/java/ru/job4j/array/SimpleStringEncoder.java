30d1e5d5cef56d33e24a9ab0bce2933408113a73
Sumyork <psychohistorian.p@gmail.com>
2024-02-01 11:53:16 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049 #477699] - исправлен

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -6,15 +6,15 @@ public class SimpleStringEncoder {
         char symbol = input.charAt(0);
         int counter = 1;
         for (int i = 1; i < input.length(); i++) {
-            if (symbol != input.charAt(i) && counter == 1) {
+            if (symbol == input.charAt(i)) {
+                counter++;
+            } else {
                 result += String.valueOf(symbol);
-                symbol = input.charAt(i);
-            } else if (symbol != input.charAt(i) && counter != 1) {
-                result += String.valueOf(symbol) + counter;
+                if (counter != 1) {
+                    result += counter;
+                }
                 symbol = input.charAt(i);
                 counter = 1;
-            } else {
-                counter++;
             }
         }
         if (counter == 1) {
